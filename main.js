406ae532a4704ca0cd2064bab26ef35ba9f05d13
Arianna Foi <ariannafoi@Air-di-Arianna.homenet.telecomitalia.it>
2023-09-24 12:49:13 +0200

    modifiche in js

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 let scritta = document.querySelector('#scritta');
-let changeTextHome = document.querySelector('.home');
+let changeTextHomeNav = document.querySelector('#homenav');
+let changeTextHome = document.querySelector('#home');
 let changeTextGallery = document.querySelector('#gallery');
 let changeTextAbout = document.querySelector('#about');
 let changeTextContact = document.querySelector('#contact');
 
+changeTextHomeNav.addEventListener('click', ()=>{
+    scritta.textContent = 'Home'
+    console.log('home')
+})
 
 changeTextHome.addEventListener('click', ()=>{
     scritta.textContent = 'Home'
